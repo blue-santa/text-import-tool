5122bc53984f225c2525ef874bf64ad6d4378013
Blue Santa <contact@bluesanta.io>
2023-11-12 15:20:56 -0900

    command-cp force copies all files from a backup directory

--- scripts/text-capture/main.cpp
+++ scripts/text-capture/main.cpp
@@ -12,21 +12,26 @@ int main() {
     // Generate new log file
     LogFile log_file;
 
+    // Declare new working file
     WorkingFile working_file;
 
+    // Test if any files are missing and, if so, auto generate them
     working_file.autoInitializeFiles(log_file);
 
-    // To Do: loadNextWorkingFile should be separate from checking if we need to reset or pick up where we left off
-
+    // Process files until the range is met or user interrupts
     while (true) {
 
+        // Load to the next working file
         if (!working_file.loadNextWorkingFile(log_file)) {
             break;
         }
 
+        // Process the next element
         working_file.processNewElement(log_file);
 
         // Pause for user to verify that all proceeded as planned
+        string prompt = "Please verify that the most recent file is correct on disk";
+        captureUserString(prompt);
 
     }
 
